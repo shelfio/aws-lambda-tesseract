1035dc99cc509525edaea59085117a4e56588171
Vlad Holubiev <golubev.vld@gmail.com>
2018-10-05 13:43:51 +0300

    test: cover isSupportedFile helper with cases

---
+++ src/index.test.js
@@ -0,0 +1,23 @@
+const {isSupportedFile} = require('./');
+
+describe('#isSupportedFile', () => {
+  it('should export isSupportedFile function', () => {
+    expect(isSupportedFile).toBeInstanceOf(Function);
+  });
+
+  it.each`
+    filePath            | expected
+    ${'/tmp/file.mp3'}  | ${false}
+    ${'/tmp/file.docx'} | ${false}
+    ${'/tmp/file'}      | ${false}
+    ${''}               | ${false}
+    ${'/tmp/file.jpg'}  | ${true}
+    ${'/tmp/file.png'}  | ${true}
+    ${'/tmp/file.svg'}  | ${false}
+    ${'/tmp/file.gif'}  | ${false}
+    ${'/tmp/file.psd'}  | ${false}
+    ${'/tmp/file.eps'}  | ${false}
+  `('should return $expected for file $filePath', ({filePath, expected}) => {
+    expect(isSupportedFile(filePath)).toEqual(expected);
+  });
+});
