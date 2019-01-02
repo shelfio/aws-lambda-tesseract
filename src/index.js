641f7206d23db98e2e27499a67dad9a51543d710
Levi Nunnink <levi@droplr.com>
2019-01-02 14:01:29 -0800

    Updates the node library so words and bounds can be extracted using tesseract. Also adds support for streams instead of only local file paths.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,82 @@
-const {unpack} = require('@shelf/aws-lambda-brotli-unpacker');
-const {execFileSync, execSync} = require('child_process');
+const {execFile} = require('child_process');
 const path = require('path');
+const fs = require('fs');
 const isImage = require('is-image');
+const {unpack} = require('@shelf/aws-lambda-brotli-unpacker');
 
 const unsupportedExtensions = new Set(['ai', 'emf', 'eps', 'gif', 'ico', 'psd', 'svg']);
 const inputPath = path.join(__dirname, '..', 'bin', 'tt.tar.br');
 const outputPath = '/tmp/tesseract/tesseract';
 
+async function runTesseract(file, opts) {
+  const ttBinary = process.env.TESSERACT_BINARY_PATH || (await unpack({inputPath, outputPath}));
+  let processFile = 'stdin';
+  if (typeof file === 'string' && fs.existsSync(file)) processFile = file;
+  if (!file) processFile = false;
+
+  const options = {
+    env: {}
+  };
+  if (!process.env.TESSERACT_BINARY_PATH) {
+    options.env.LD_LIBRARY_PATH =
+      `${process.env.LD_LIBRARY_PATH}:/tmp/tesseract/lib` || `/tmp/tesseract/lib`;
+    options.env.TESSDATA_PREFIX = process.env.TESSDATA_PREFIX || `/tmp/tesseract/tessdata`;
+  }
+  console.log('Running with options: ', options, opts);
+  if (!process.env.TESSERACT_BINARY_PATH) options.cwd = '/tmp/tesseract';
+  return new Promise((resolve, reject) => {
+    const finalOpts = processFile ? [processFile, ...opts] : opts;
+    const child = execFile(ttBinary, finalOpts, options, (error, stdout, stderr) => {
+      if (error) return reject(error);
+      return resolve(stdout);
+    });
+    if (processFile === 'stdin') file.pipe(child.stdin);
+  });
+}
+
+function isUnsupportedFileExtension(filePath) {
+  const ext = path
+    .extname(filePath)
+    .slice(1)
+    .toLowerCase();
+
+  return unsupportedExtensions.has(ext);
+}
+
+function tsvJSON(tsv) {
+  const lines = tsv.split('\n');
+  const result = [];
+  const headers = lines[0].split('\t');
+
+  for (let i = 1; i < lines.length; i++) {
+    const obj = {};
+    const currentline = lines[i].split('\t');
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j];
+    }
+    result.push(obj);
+  }
+  return result;
+}
+
 module.exports.getExecutablePath = async function() {
   return unpack({inputPath, outputPath});
 };
 
-module.exports.getTextFromImage = async function(filePath) {
-  const ttBinary = await unpack({inputPath, outputPath});
+module.exports.getTextFromImage = async function(file) {
+  const result = await runTesseract(file, ['stdout', '-l', 'eng']);
+  return result.toString();
+};
 
-  const stdout = execFileSync(ttBinary, [filePath, 'stdout', '-l', 'eng'], {
-    cwd: '/tmp/tesseract',
-    env: {
-      LD_LIBRARY_PATH: './lib',
-      TESSDATA_PREFIX: './tessdata'
-    }
-  });
+module.exports.getWordsAndBounds = async function(file) {
+  const result = await runTesseract(file, ['stdout', '-l', 'eng', 'tsv']);
+  const object = tsvJSON(result.toString());
+  return object;
+};
 
-  execSync(`rm ${filePath}`);
-
-  return stdout.toString();
+module.exports.version = async function() {
+  const result = await runTesseract(false, ['--version']);
+  return result.toString();
 };
 
 module.exports.isSupportedFile = function(filePath) {
@@ -35,12 +87,3 @@ module.exports.isSupportedFile = function(filePath) {
 
   return !isUnsupportedFileExtension(filePath);
 };
-
-function isUnsupportedFileExtension(filePath) {
-  const ext = path
-    .extname(filePath)
-    .slice(1)
-    .toLowerCase();
-
-  return unsupportedExtensions.has(ext);
-}
