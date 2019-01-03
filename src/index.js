1b74bf3b14fdd90fdf03e1c28b1e6f5b3ef998ee
Levi Nunnink <levi@droplr.com>
2019-01-03 10:13:38 -0800

    Update tsv parsing to use stable library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const {execFile} = require('child_process');
 const path = require('path');
 const fs = require('fs');
+const parseCSV = require('csv-parse');
 const isImage = require('is-image');
 const {unpack} = require('@shelf/aws-lambda-brotli-unpacker');
 
@@ -22,7 +23,6 @@ async function runTesseract(file, opts) {
       `${process.env.LD_LIBRARY_PATH}:/tmp/tesseract/lib` || `/tmp/tesseract/lib`;
     options.env.TESSDATA_PREFIX = process.env.TESSDATA_PREFIX || `/tmp/tesseract/tessdata`;
   }
-  console.log('Running with options: ', options, opts);
   if (!process.env.TESSERACT_BINARY_PATH) options.cwd = '/tmp/tesseract';
   return new Promise((resolve, reject) => {
     const finalOpts = processFile ? [processFile, ...opts] : opts;
@@ -43,22 +43,6 @@ function isUnsupportedFileExtension(filePath) {
   return unsupportedExtensions.has(ext);
 }
 
-function tsvJSON(tsv) {
-  const lines = tsv.split('\n');
-  const result = [];
-  const headers = lines[0].split('\t');
-
-  for (let i = 1; i < lines.length; i++) {
-    const obj = {};
-    const currentline = lines[i].split('\t');
-    for (let j = 0; j < headers.length; j++) {
-      obj[headers[j]] = currentline[j];
-    }
-    result.push(obj);
-  }
-  return result;
-}
-
 module.exports.getExecutablePath = async function() {
   return unpack({inputPath, outputPath});
 };
@@ -70,7 +54,12 @@ module.exports.getTextFromImage = async function(file) {
 
 module.exports.getWordsAndBounds = async function(file) {
   const result = await runTesseract(file, ['stdout', '-l', 'eng', 'tsv']);
-  const object = tsvJSON(result.toString());
+  const object = await new Promise((resolve, reject) =>
+    parseCSV(result.toString(), {delimiter: '\t', columns: true}, (err, result) => {
+      if (err) return reject(err);
+      return resolve(result);
+    })
+  );
   return object;
 };
 
