504f51ece5adf9be895ea6901e1146689695f3f7
terret <andrew.petlya@gmail.com>
2020-01-15 20:08:52 +0200

    feat: update files index/test.index .js -> .ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
-const {extract} = require('tar');
-const {execFileSync, execSync} = require('child_process');
-const path = require('path');
-const isImage = require('is-image');
+import {extract} from 'tar';
+import {execFileSync, execSync} from 'child_process';
+import path from 'path';
+import isImage from 'is-image';
 
-const unsupportedExtensions = new Set(['ai', 'emf', 'eps', 'gif', 'ico', 'psd', 'svg']);
+const unsupportedExtensions: Set<string> = new Set([
+  'ai',
+  'emf',
+  'eps',
+  'gif',
+  'ico',
+  'psd',
+  'svg'
+]);
 const inputPath = path.join(__dirname, '..', 'bin', 'tt.tar.gz');
 const outputPath = '/tmp/tesseract-standalone/tesseract';
 
-module.exports.getExecutablePath = async function() {
+export async function getExecutablePath(): Promise<string> {
   await extract({file: inputPath, cwd: '/tmp'});
 
   return outputPath;
-};
+}
 
-module.exports.getTextFromImage = async function(filePath) {
+export async function getTextFromImage(filePath: string): Promise<string> {
   await extract({file: inputPath, cwd: '/tmp'});
 
   const stdout = execFileSync(outputPath, [filePath, 'stdout', '-l', 'eng'], {
@@ -27,18 +35,18 @@ module.exports.getTextFromImage = async function(filePath) {
   execSync(`rm ${filePath}`);
 
   return stdout.toString();
-};
+}
 
-module.exports.isSupportedFile = function(filePath) {
+export function isSupportedFile(filePath): boolean {
   // Reject all non-image files for OCR
   if (!isImage(filePath)) {
     return false;
   }
 
   return !isUnsupportedFileExtension(filePath);
-};
+}
 
-function isUnsupportedFileExtension(filePath) {
+function isUnsupportedFileExtension(filePath): boolean {
   const ext = path
     .extname(filePath)
     .slice(1)
