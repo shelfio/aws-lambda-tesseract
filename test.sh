ef791d20ff80238ca560c2dbcad2b66bbcebed9d
Vlad Holubiev <golubev.vld@gmail.com>
2022-06-06 22:43:37 +0200

    feat: upgrade test.sh code to use node 16 iamge

--- test.sh
+++ test.sh
@@ -6,6 +6,11 @@ cd ..
 
 yarn babel test.ts --out-file test.js
 
+(sleep 5; curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"payload":"hello world!"}') &
+
+
 docker run --rm \
   -v "$PWD":/var/task \
-  lambci/lambda:nodejs12.x test.handler
+  -p 9000:8080 \
+  public.ecr.aws/lambda/nodejs:16 \
+  test.handler
